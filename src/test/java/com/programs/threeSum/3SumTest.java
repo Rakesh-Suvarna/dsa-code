383a0a39cbe3fb6ec99ffc07d24d35b68fa94f84
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-23 12:52:15 +0530

    fix

--- src/test/java/com/programs/threeSum/3SumTest.java
+++ src/test/java/com/programs/threeSum/3SumTest.java
@@ -8,19 +8,18 @@ public class 3SumTest{
     @Test
     public void test3Sum(){
         
-        int[] nums = new int[]{-1,0,1,2,-1,-4]};
+        int[] nums = new int[]{-1,0,1,2,-1,-4};
         List<List<Integer>> result = new Solution().threeSum(nums);
 
         List<List<Integer>> expectedOutput = new ArrayList<List<Integer>>();
-        [[-1,-1,2],[-1,0,1]]
         List<Integer> trp1 = new ArrayList<Integer>();
-        trp1.add(-1);
-        trp1.add(-1);
-        trp1.add(2);
+        trp1.add(new Integer(-1));
+        trp1.add(new Integer(-1));
+        trp1.add(new Integer(2));
         List<Integer> trp2 = new ArrayList<Integer>();
-        trp2.add(-1);
-        trp2.add(0);
-        trp2.add(1);
+        trp2.add(new Integer(-1));
+        trp2.add(new Integer(0));
+        trp2.add(new Integer(1));
         expectedOutput.add(trp1);
         expectedOutput.add(trp2);
         
