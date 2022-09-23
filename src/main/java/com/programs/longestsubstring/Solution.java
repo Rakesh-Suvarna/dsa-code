2958e2e117584e21806411577b31c935baf7dc63
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-23 13:47:25 +0530

    Longest Substring Without Repeating Characters

---
+++ src/main/java/com/programs/longestsubstring/Solution.java
@@ -0,0 +1,12 @@
+package com.programs.longestsubstring;
+
+public class Solution{
+
+    public int lengthOfLongestSubstring(String s) {
+        
+        int result = 0;
+        //write code here
+
+        return result;
+    }
+}
--- src/test/java/com/programs/AllTest.java
+++ src/test/java/com/programs/AllTest.java
@@ -18,4 +18,20 @@ public class AllTest{
         Assert.assertEquals(new com.programs.containerwithmostwater.Solution().maxArea(new int[]{1,1})
         , 1);
     }
+
+    @Test
+    public void testLongestSubstring(){
+
+        Assert.assertEquals(new com.programs.longestsubstring.Solution().lengthOfLongestSubstring("abcabcbb")
+        , 3);
+    }
+
+    @Test
+    public void test2LongestSubstring(){
+
+        Assert.assertEquals(new com.programs.longestsubstring.Solution().lengthOfLongestSubstring("bbbbb")
+        , 1);
+        Assert.assertEquals(new com.programs.longestsubstring.Solution().lengthOfLongestSubstring("pwwkew")
+        , 3);
+    }
 }
