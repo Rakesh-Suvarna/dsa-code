6dd9ba5a4d76292bdfc36e6e7bb192f27b6d703c
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-23 13:15:05 +0530

    fix

--- src/test/java/com/programs/threeSum/ThreeSumTest.java
+++ src/test/java/com/programs/threeSum/ThreeSumTest.java
@@ -12,7 +12,7 @@ public class ThreeSumTest{
         int[] nums = new int[]{-1,0,1,2,-1,-4};
         List<List<Integer>> result = new Solution().threeSum(nums);
 
-        int[][] expectedOutput = new int[][]{{-1, -1, 2}, {-1, 0, 1}}
+        int[][] expectedOutput = new int[][]{{-1, -1, 2}, {-1, 0, 1}};
         
         int[][] array = new int[result.size()][];
         int[] blankArray = new int[0];
