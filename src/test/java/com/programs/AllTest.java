e6f759b008a7766e93966868f668be7842d11237
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-23 13:13:35 +0530

    fix

--- src/test/java/com/programs/AllTest.java
+++ src/test/java/com/programs/AllTest.java
@@ -8,14 +8,14 @@ public class AllTest{
     @Test
     public void testMaxArea(){
 
-        Assert.assertEquals(new com.programs.containerwithmostwater.Solution.maxArea(new int[]{1,8,6,2,5,4,8,3,7})
+        Assert.assertEquals(new Solution().maxArea(new int[]{1,8,6,2,5,4,8,3,7})
         , 49);
     }
 
     @Test
     public void test2MaxArea(){
 
-        Assert.assertEquals(new com.programs.containerwithmostwater.Solution.maxArea(new int[]{1,1})
+        Assert.assertEquals(new Solution().maxArea(new int[]{1,1})
         , 1);
     }
 }
--- src/test/java/com/programs/threeSum/ThreeSumTest.java
+++ src/test/java/com/programs/threeSum/ThreeSumTest.java
@@ -12,18 +12,13 @@ public class ThreeSumTest{
         int[] nums = new int[]{-1,0,1,2,-1,-4};
         List<List<Integer>> result = new Solution().threeSum(nums);
 
-        List<List<Integer>> expectedOutput = new ArrayList<List<Integer>>();
-        List<Integer> trp1 = new ArrayList<Integer>();
-        trp1.add(new Integer(-1));
-        trp1.add(new Integer(-1));
-        trp1.add(new Integer(2));
-        List<Integer> trp2 = new ArrayList<Integer>();
-        trp2.add(new Integer(-1));
-        trp2.add(new Integer(0));
-        trp2.add(new Integer(1));
-        expectedOutput.add(trp1);
-        expectedOutput.add(trp2);
+        int[][] expectedOutput = new int[][]{{-1, -1, 2}, {-1, 0, 1}}
         
-        Assert.assertArraysEquals(result, expectedOutput);
+        int[][] array = new int[result.size()][];
+        int[] blankArray = new int[0];
+        for(int i=0; i < results.size(); i++) {
+            array[i] = results.get(i).toArray(blankArray);
+        }
+        Assert.assertArrayEquals(array., expectedOutput);
     }
 }
