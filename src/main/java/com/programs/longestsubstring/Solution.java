44317c14029030cb2c8ecb85857e1881fe98b34c
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-24 19:34:35 +0530

    longestsubstring solution

--- src/main/java/com/programs/longestsubstring/Solution.java
+++ src/main/java/com/programs/longestsubstring/Solution.java
@@ -5,7 +5,22 @@ public class Solution{
     public int lengthOfLongestSubstring(String s) {
         
         int result = 0;
-        //write code here
+        int start=0;
+        int end=0;
+        Set<Character> set = new HashSet<Character>();
+        while(end < s.length()){
+
+            Character ch = s.charAt(end);
+            if(set.contains(ch)){
+
+                set.add(ch);
+                result = Math.max(result, set.size());
+                end++;
+            }else{
+                set.remove(s.charAt(start));
+                start++;
+            }
+        }
 
         return result;
     }
