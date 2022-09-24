2522edab1e7db09b5dde8d1fb2bdb72b5c0996d9
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-24 20:58:32 +0530

    lengthOfLongestSubstringKDistinct solution

--- src/main/java/com/programs/longestsubstring/SolutionQn3.java
+++ src/main/java/com/programs/longestsubstring/SolutionQn3.java
@@ -6,7 +6,26 @@ public class SolutionQn3{
 
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
 
+        int firstIdx=0;
         int result = 0;
+        Map<Character, Integer> window = new HashMap<Character, Integer>();
+
+        for(int i=0;i<s.length();i++){
+
+            Character ch = s.charAt(i);
+            window.put(ch, window.getOrDefault(ch, 0)+1);
+            if(window.size() > k){
+                Character chAtFrstIdx = s.charAt(firstIdx);
+                int val = window.get(chAtFrstIdx); //move the window
+                if(val == 0){
+                    window.remove(chAtFrstIdx);
+                }else{
+                    window.put(chAtFrstIdx, --val);
+                }
+                firstIdx++;
+            }
+            result = Math.max(result, i - firstIdx + 1);
+        }
         return result;
     }
 }
