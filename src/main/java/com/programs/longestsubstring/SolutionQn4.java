46771c534409b3ce52700d5d9e71a237ae14ac18
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-25 13:18:47 +0530

    slidingwindowmaximum solution

--- src/main/java/com/programs/longestsubstring/SolutionQn4.java
+++ src/main/java/com/programs/longestsubstring/SolutionQn4.java
@@ -2,14 +2,37 @@ package com.programs.longestsubstring;
 
 import java.util.*;
 
-public class SolutionQn2{
+public class SolutionQn4{
 
     /**
      * @param nums: A list of integers.
      * @param k: An integer
      * @return: The maximum number inside the window at each moving.
      */
-    public List<Integer> maxSlidingWindow(int[] nums, int k) {
-        // write your code here
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        
+        int[] result = new int[nums.length-k+1];
+        int c = 0;
+        //define a max heap using Priority queue
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(
+            Collections.reverseOrder()
+        ); 
+        //add k elements to pq
+        for(int j=0;j<k;j++){
+
+            pq.add(nums[j]);
+        }
+        //get maximum for each window and move the window and update heap
+        for(int i=k;i<nums.length;i++){
+            result[c++] = pq.peek();
+            //remove i-k th element
+            pq.remove(nums[i-k]);
+            //add i th element to pq
+            pq.add(nums[i]);
+        }
+        //add the max for last window
+        result[c] = pq.peek();
+
+        return result;
     }
 }
--- src/test/java/com/programs/AllTest.java
+++ src/test/java/com/programs/AllTest.java
@@ -51,4 +51,13 @@ public class AllTest{
         Assert.assertEquals(new com.programs.longestsubstring.SolutionQn3().lengthOfLongestSubstringKDistinct("WORLD", 4)
         , 4);
     }
+
+    @Test
+    public void maxSlidingWindow(){
+
+        Assert.assertEquals(new com.programs.longestsubstring.SolutionQn4().maxSlidingWindow(new int[]{1,2,7,7,8}, 3)
+        , new int[]{7,7,8});
+        Assert.assertEquals(new com.programs.longestsubstring.SolutionQn4().maxSlidingWindow(new int[]{1,2,3,1,2,3]}, 5)
+        , new int[]{3,3});
+    }
 }
