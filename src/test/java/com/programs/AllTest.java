b65292de84e377d7224bc82f3be6c258a253eee3
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-26 20:52:42 +0530

    fix

--- src/test/java/com/programs/AllTest.java
+++ src/test/java/com/programs/AllTest.java
@@ -88,6 +88,6 @@ public class AllTest{
         Assert.assertArrayEquals(new com.programs.kthclosestptstoorigin.KthClosest().kClosest(new int[][]{{1,3},{-2,2}}, 1)
         , new int[][]{{-2, 2}});
         Assert.assertArrayEquals(new com.programs.kthclosestptstoorigin.KthClosest().kClosest(new int[][]{{3,3},{5,-1},{-2,4}}, 2)
-        , new int[][]{{3,3},{-2,4}});
+        , new int[][]{{-2,4},{3,3}});
     }
 }
