8067912052dffacd033fda4909aea0514f6adcc3
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-26 20:50:04 +0530

    fix

--- src/main/java/com/programs/kthclosestptstoorigin/KthClosest.java
+++ src/main/java/com/programs/kthclosestptstoorigin/KthClosest.java
@@ -2,7 +2,7 @@ package com.programs.kthclosestptstoorigin;
 
 import java.util.*;
 
-class KthClosest {
+public class KthClosest {
     public int[][] kClosest(int[][] points, int k) {
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> y[0] * y[0] + y[1] * y[1] - x[0] * x[0] - x[1] * x[1]);//y[0]^2 + y[1]^2 - x[0]^2 - x[1]^2 
