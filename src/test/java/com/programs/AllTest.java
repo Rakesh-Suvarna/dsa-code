7e86c06de2c5e48a3df1711f81dcdb0d188d29db
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-26 20:43:32 +0530

    KthLargest

--- src/test/java/com/programs/AllTest.java
+++ src/test/java/com/programs/AllTest.java
@@ -82,4 +82,12 @@ public class AllTest{
         Assert.assertEquals(obj.add(9)
         , 8);
     }
+
+    @Test
+    public void KthLargest(){
+        Assert.assertArrayEquals(package com.programs.kthclosestptstoorigin.KthClosest.kClosest({{1,3},{-2,2}}, 1)
+        , new int[][]{{-2, 2}});
+        Assert.assertArrayEquals(package com.programs.kthclosestptstoorigin.KthClosest.kClosest({{3,3},{5,-1},{-2,4}}, 2)
+        , new int[][]{{3,3},{-2,4}});
+    }
 }
