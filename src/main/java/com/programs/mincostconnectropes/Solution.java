67583b73cd4933a3dd7efcca6d024d8072331957
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-26 12:13:28 +0530

    adding elements to pq

--- src/main/java/com/programs/mincostconnectropes/Solution.java
+++ src/main/java/com/programs/mincostconnectropes/Solution.java
@@ -6,13 +6,19 @@ public class Solution {
     
     public int minCost(int arr[]){
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>(arr);
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        //add all elements to the queue to form a min heap
+        for(int i=0; i<arr.length; i++){
+            pq.add(arr[i]);
+        }
         int cost = 0;
         while(pq.size() > 1){
             //pop top 2 elements and calculate sum
             int x = pq.poll();
             int y = pq.poll();
             int sum = x+y;
+            //add the current sum to queue
             pq.add(sum);
             cost += sum;
         }
