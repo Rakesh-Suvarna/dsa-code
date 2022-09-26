b0f1c8b02009074a5127db81cec336e81e31628c
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-26 21:06:31 +0530

    reorderLogFiles

---
+++ src/main/java/com/programs/reorderdatalogs/ReorderDatalogs.java
@@ -0,0 +1,11 @@
+package com.programs.reorderdatalogs;
+
+import java.util.*;
+
+public class ReorderDatalogs {
+     
+    public String[] reorderLogFiles(String[] logs) {
+        
+        return logs;
+    }
+}
