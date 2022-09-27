eccb94c429535c59dba52f9c117efb61647fe3aa
Rakesh Marakala S <66661398+Rakesh-Suvarna@users.noreply.github.com>
2022-09-27 11:13:04 +0530

    ReorderDatalogs

--- src/main/java/com/programs/reorderdatalogs/Reorder data logs files.md
+++ src/main/java/com/programs/reorderdatalogs/Reorder data logs files.md
@@ -25,7 +25,6 @@ Example 2:
 
 Input: logs = ["a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo"]
 Output: ["g1 act car","a8 act zoo","ab1 off key dog","a1 9 2 3 1","zo4 4 7"]
- 
 
 Constraints:
 
--- src/main/java/com/programs/reorderdatalogs/ReorderDatalogs.java
+++ src/main/java/com/programs/reorderdatalogs/ReorderDatalogs.java
@@ -6,6 +6,31 @@ public class ReorderDatalogs {
      
     public String[] reorderLogFiles(String[] logs) {
         
+        Comparator<String> comp = new Comparator<String>(){
+            @Overide
+            public int compare(String s1, String s2){
+
+                int s1FirstIndex = s1.indexOf(" ");
+                int s2FirstIndex = s2.indexOf(" ");
+                if(s1.charAt(s1FirstIndex+1) <= '9'){//digit
+                    if(s2.charAt(s2FirstIndex+1) <= '9'){
+                        return 0;//both are digits return 0 - indicates maintain the order for digits
+                    }else{
+                        return 1;
+                    }
+                }
+                if(s2.charAt(s2FirstIndex+1) <= 9){
+                    return -1;
+                }
+                //string comparision
+                int preCompute = s1.substring(s1FirstIndex+1).compare(s2.substring(s2FirstIndex+1));//The letter-logs are sorted lexicographically by their contents.
+                if(preCompute ==0) {//If their contents are the same, then sort them lexicographically by their identifiers. 
+                    return s1.substring(0, s1FirstIndex).compare(s2.substring(0, s2FirstIndex));
+                }
+                return preCompute;
+            }
+        };
+        Arrays.sort(logs, comp);
         return logs;
     }
 }
